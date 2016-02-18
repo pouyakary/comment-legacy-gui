cae0335beef0a8560f22e98562d1cb181f78cbbf
Pouya Kary <k@karyfoundation.org>
2016-02-18 19:33:39 +0330

    build file

--- build.sh
+++ build.sh
@@ -8,7 +8,19 @@ echo "    [ done ]"
 
 # Minifing the Code
 echo "--> Minifing the Core"
-uglifyjs comment.js --o "Comment IV/comment.min.js" -m
+uglifyjs -m -o Resources/Comment.js Resources/Comment.js
 echo "    [ done ]"
 
+# Bulding the Style Sheets
+echo "--> Builing the Style Sheets"
+lessc Styles/Styles.less Resources/styles.css
+echo "    [ done ]"
 
+# Copying the Resource Files
+for file in Resources/*
+do 
+	echo " -> Copying ${file}"
+	name=${file##*/}
+	cp $file "Comment IV/${name}"
+	echo "    [ done ]"
+done
