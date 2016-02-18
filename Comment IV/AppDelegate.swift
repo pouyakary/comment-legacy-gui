43b18459e1ea8d4e3aa7c8558a3279d20628add9
Pouya Kary <k@karyfoundation.org>
2016-02-18 18:12:09 +0330

    Initial Commit

---
+++ Comment IV/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Comment IV
+//
+//  Created by Pouya Kary on 2/18/16.
+//  Copyright (c) 2016 Kary Foundation, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Comment IVTests/Comment_IVTests.swift
@@ -0,0 +1,36 @@
+//
+//  Comment_IVTests.swift
+//  Comment IVTests
+//
+//  Created by Pouya Kary on 2/18/16.
+//  Copyright (c) 2016 Kary Foundation, Inc. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Comment_IVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
