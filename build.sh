9aadda8f594e7472853c33bbfa96e0cefd5789cd
Pouya Kary <k@karyfoundation.org>
2016-02-18 19:56:02 +0330

    now is better

--- build.sh
+++ build.sh
@@ -1,26 +1,41 @@
+#
+# Comment IV 
+#    Copyright 2016 Kary Foundation, Inc.
+#    Author: Pouya Kary <k@karyfoundation.org>
+#
 
 echo "Bulding Comment IV"
 
 # Compiling the Code
-echo "--> Compiling the Core"
+echo "-> Compiling the Core"
 tsc
-echo "    [ done ]"
+echo "   [ done ]"
 
 # Minifing the Code
-echo "--> Minifing the Core"
+echo "-> Minifing the Core"
 uglifyjs -m -o Resources/Comment.js Resources/Comment.js
-echo "    [ done ]"
+echo "   [ done ]"
 
 # Bulding the Style Sheets
-echo "--> Builing the Style Sheets"
+echo "-> Builing the Style Sheets"
 lessc Styles/Styles.less Resources/styles.css
-echo "    [ done ]"
+echo "   [ done ]"
 
 # Copying the Resource Files
+echo "-> Copying the files"
 for file in Resources/*
 do 
-	echo " -> Copying ${file}"
+	echo "    -> Copying ${file}"
 	name=${file##*/}
 	cp $file "Comment IV/${name}"
-	echo "    [ done ]"
 done
+echo "   [ done ]"
+
+# Compiling the Swift View
+echo "-> Compiling the Swift View"
+xcodebuild -list -project "Comment IV.xcodeproj"
+echo "   [ done ]"
+
+
+# Done
+echo "Finished"
