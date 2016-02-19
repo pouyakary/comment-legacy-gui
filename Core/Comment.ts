0e9481f9006ea37046b9925a762143824c658223
Pouya Kary <k@karyfoundation.org>
2016-02-19 22:48:33 +0330

    adding the last comment

--- Core/Comment.ts
+++ Core/Comment.ts
@@ -63,6 +63,47 @@ module Core {
 		}
 		
 	//
+	// ─── FLAG COMMENT GENERATOR ─────────────────────────────────────────────────────
+	//
+
+		export function GenerateFlagCommet ( ) : string {
+			// Line 1
+			var comment = languageOneLine + '\n';
+			// Line 2
+			const text = MakeTitle( globalTextValue );
+			comment += languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , text.length + 40 ) + ' ' + globalIndexValue + ' ' + MakeRepeat( boxHorizontalCharacter , 10 ) + '\n';
+			// Line 3
+			comment += languageOneLine + '  ::::::' + text + ' : :  :   :    :     :        :          :\n';
+			// Line 4
+			comment += languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , 50 + text.length ) + '\n';
+			// Line 5
+			comment += languageOneLine
+			// Done
+			return comment;
+		}
+
+	//
+	// ─── LINE GENERATOR ─────────────────────────────────────────────────────────────
+	//
+
+		export function GenerateLineComment ( ) : string {
+			return languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , globalSizeValue );
+		}
+		
+	//
+	// ─── SUBLINE COMMENT GENERATOR ──────────────────────────────────────────────────
+	//
+
+		export function GenerateSubLineComment ( ) : string {
+			var comment = languageOneLine;
+			if ( globalSizeValue % 2 == 0 ) {
+				comment += ' ';
+			}
+			comment += MakeRepeat ( ' -' , globalSizeValue / 2 );
+			return comment;
+		}
+		
+	//
 	// ─── SECTION COMMENT GENERATOR ──────────────────────────────────────────────────
 	//
 
