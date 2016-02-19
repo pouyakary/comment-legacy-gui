9f984b8e6000cd5e426569d743ac525ed5803764
Pouya Kary <k@karyfoundation.org>
2016-02-19 18:14:53 +0330

    styles added

--- Core/Comment.ts
+++ Core/Comment.ts
@@ -4,6 +4,24 @@
 //    Author: Pouya Kary <k@karyfoundation.org>
 //
 
-module Comment {
+var languageMultiLineTopLeft: string;
+var languageMultiLineTopRight: string;
+var languageMultiLineBottomLeft: string;
+var languageMultiLineBottomRight: string;
+var languageOneLine: string;
+
+module Core {
 	
+	const boxVerticalCharacter 		= '&boxv;';
+	const boxHorizontalCharacter 	= '&boxh;';
+	const boxTopLeftCharacter 		= '&boxdr;';
+	const boxTopRightCharacter 		= '&boxdl;';
+	const boxBottomLeftCharacter 	= '&boxur;';
+	const boxBottomRightCharacter	= '&boxul;';
+	
+	export function GenerateClassComment ( commentText: string , size: number ) {
+		var commnet: string = '';
+		
+	}
+
 }
