fea84da1e07191893752fa1f096d52b950e5e536
Pouya Kary <k@karyfoundation.org>
2016-02-28 20:16:10 +0330

    namespace changes

---
+++ Core/kf.text.numerics.ts
@@ -0,0 +1,63 @@
+//
+// Kary.Text Framework - Numerics Library
+//    Copyright 2015-2016 Kary Foundation, Inc.
+//    Author: Pouya Kary <k@karyfoundation.org>
+//
+
+
+/* ──────────────────────────────────────────────────────────────────────────────── *
+ * ::::::::: K A R Y   F R A M E W O R K   N U M E R I C S   L I B R A R Y :::::::: *
+ * ──────────────────────────────────────────────────────────────────────────────── */
+	
+// ────────────────────────────────────────────────────────────────────────────────────────────────────	
+	
+	module Kary.Text.Numerics {
+		
+		//
+		// ─── ROMAN NUMBER GENERATOR ─────────────────────────────────────────────────────
+		//
+
+			/**
+			 * Generates a Roman number in by inputs in range of [0..4999]
+			 */
+			export function Roman ( input: number ) : string {
+				// A very special case as you know...
+				if ( input === 0 ) {
+					return 'NULL';
+				}
+				
+				// Also known...
+				if ( input > 4999 ) {
+					return 'HUGE';
+				}
+				
+				// Result
+				var result: string = '';
+				
+				// Values:
+				const values: Array<number> = [ 
+					1000 , 900 , 500 , 400 , 100 , 90 , 
+					50 , 40 , 10 , 9 , 5 , 4 , 1 
+				];
+				const numerals: Array<string> = [ 
+					"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , 
+					"XL" , "X" , "IX" , "V" , "IV" , "I" 
+				];
+				
+				// Generator 
+				for ( var index = 0 ; index < 13 ; index++ ) {
+					while ( input >= values[ index ] ) {
+						input -= values[ index ];
+						result += numerals[ index ];
+					}
+				}
+				
+				// done
+				return result;
+			}
+		
+		// ────────────────────────────────────────────────────────────────────────────────
+		
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
