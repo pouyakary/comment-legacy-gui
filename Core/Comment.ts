b42854d7b96be162af886afc633558cf7708dfd4
Pouya Kary <k@karyfoundation.org>
2016-02-28 20:11:15 +0330

    cleaned the code

--- Core/Comment.ts
+++ Core/Comment.ts
@@ -4,216 +4,255 @@
 //    Author: Pouya Kary <k@karyfoundation.org>
 //
 
-/// <reference path="kf.ts" />
+/// <reference path="kf.text.numerics.ts" />
 
 
-var languageMultiLineTopLeft: string;
-var languageMultiLineTopRight: string;
-var languageMultiLineBottomLeft: string;
-var languageMultiLineBottomRight: string;
-var languageOneLine: string;
+//
+// ──────────────────────────────────────────────────────────────────────── I ──────────
+//  :::::: S H A R E D   V A R I A B L E S : :  :   :    :     :        :          :
+// ──────────────────────────────────────────────────────────────────────────────────
+//
 
-var globalTextValue: string;
-var globalSeparatorValue: string;
-var globalSizeValue: number;
-var globalIndexValue: number;
-var globalIndentSizeValue: number;
-var globalIndentStringValue: string;
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
 
-var errorPlaceHolder: string;
-
-module Core {
-	
 	//
-	// ─── SPECIAL CHARACTERS ─────────────────────────────────────────────────────────
+	// ─── LANGUAGE SETTINGS ──────────────────────────────────────────────────────────
 	//
 
-		const boxVerticalCharacter 		= '&boxv;';
-		const boxHorizontalCharacter 	= '&boxh;';
-		const boxTopLeftCharacter 		= '&boxdr;';
-		const boxTopRightCharacter 		= '&boxdl;';
-		const boxBottomLeftCharacter 	= '&boxur;';
-		const boxBottomRightCharacter	= '&boxul;';
+		var languageMultiLineTopLeft: 		string;
+		var languageMultiLineTopRight: 		string;
+		var languageMultiLineBottomLeft: 	string;
+		var languageMultiLineBottomRight: 	string;
+		var languageOneLine: 				string;
 		
 	//
-	// ─── COMMENT CLASS GENERATOR ────────────────────────────────────────────────────
-	//
-
-		export function GenerateClassComment ( ) : string {
-			// First Info
-			var commnet: string;
-			
-			// Line One
-			commnet = languageMultiLineTopLeft + ' ' + MakeLine( globalSizeValue ) + ' ' + languageMultiLineTopRight + '\n';
-			
-			// Line Two
-			const titleText = MakeTitle( globalTextValue );
-			var	  inCaseOfOddNumber = '';
-			var   dots = '';
-			
-			for ( var counter = 1; counter < ( globalSizeValue - titleText.length ) / 2 ; counter++ ) {
-				dots += '&colon;';
-			}
-			
-			if ( ( globalSizeValue - titleText.length ) % 2 == 0 ) {
-				console.log( 'here');
-				inCaseOfOddNumber = '&colon;';
-			}
-			
-			commnet += languageMultiLineBottomLeft + ' ' + inCaseOfOddNumber + dots + titleText + ' ' + dots + ' ' + languageMultiLineTopRight + '\n' ;
-			
-			// Line Tree
-			commnet += languageMultiLineBottomLeft + ' ' + MakeLine( globalSizeValue ) + ' ' + languageMultiLineBottomRight;
-			
-			// Done!
-			console.log( commnet );
-			return commnet;
-		}
-		
-	//
-	// ─── FLAG COMMENT GENERATOR ─────────────────────────────────────────────────────
-	//
-
-		export function GenerateFlagCommet ( ) : string {
-			// Line 1
-			var comment = languageOneLine + '\n';
-			// Line 2
-			const text = MakeTitle( globalTextValue );
-			comment += languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , text.length + 40 ) + ' ' + Kary.Text.Numerics.Roman( globalIndexValue ) + ' ' + MakeRepeat( boxHorizontalCharacter , 10 ) + '\n';
-			// Line 3
-			comment += languageOneLine + '  ::::::' + text + ' : :  :   :    :     :        :          :\n';
-			// Line 4
-			comment += languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , 50 + text.length ) + '\n';
-			// Line 5
-			comment += languageOneLine
-			// Done
-			return comment;
-		}
-
-	//
-	// ─── LINE GENERATOR ─────────────────────────────────────────────────────────────
-	//
-
-		export function GenerateLineComment ( ) : string {
-			return languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , globalSizeValue );
-		}
-		
-	//
-	// ─── SUBLINE COMMENT GENERATOR ──────────────────────────────────────────────────
-	//
-
-		export function GenerateSubLineComment ( ) : string {
-			var comment = languageOneLine;
-			if ( globalSizeValue % 2 == 0 ) {
-				comment += ' ';
-			}
-			comment += MakeRepeat ( ' -' , globalSizeValue / 2 );
-			return comment;
-		}
-		
-	//
-	// ─── SECTION COMMENT GENERATOR ──────────────────────────────────────────────────
-	//
-
-		export function GenerateSectionComment (  ) : string {
-			// Line 1
-			var comment = languageOneLine + '\n';
-			// Line 2
-			comment += (
-				languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , 3 ) + ' ' +
-				globalTextValue.toUpperCase( ) + ' ' + MakeRepeat( boxHorizontalCharacter , globalSizeValue - globalTextValue.length - 5 ) + '\n'
-			);
-			// Line 3
-			comment += languageOneLine;
-			// Done
-			return comment;	
-		}
-		
-	//
-	// ─── SUBSECTION COMMENT GENERATOR ───────────────────────────────────────────────
+	// ─── INPUTS ─────────────────────────────────────────────────────────────────────
 	//
 	
-		export function GenerateSubSectionComment (  ) : string {
-			// Line 1
-			var comment = languageOneLine + '\n';
-			// Line 2
-			comment += languageOneLine + ' ' + '- -' + ' ' + globalTextValue.toLowerCase( );
-			var lineSize = ( globalSizeValue - globalTextValue.length - 5 ) / 2;
-			if ( lineSize % 2 == 0 ) {
-				comment += ' ' + MakeRepeat( ' -' , lineSize ) + '\n'; 
-			} else {
-				comment += MakeRepeat( ' -' , lineSize ) + '\n';
-			}
-			// Line 3
-			comment += languageOneLine;
-			// Done
-			return comment;	
-		}
+		var globalTextValue: 				string;
+		var globalSeparatorValue:			string;
+		var globalSizeValue: 				number;
+		var globalIndexValue: 				number;
+		var globalIndentSizeValue: 			number;
+		var globalIndentStringValue: 		string;
 		
 	//
-	// ─── IS SECTION COMMENT GENERATOR ───────────────────────────────────────────────
+	// ─── SHARED SYSTEM RESOURCES ────────────────────────────────────────────────────
 	//
 
-		export function GenerateInSectionComment ( ) : string {
-			var comment =  languageOneLine + '\n';
-			comment += languageOneLine + ' ' + globalTextValue.toUpperCase( ) + '\n';
-			comment += languageOneLine;
-			return comment;
-		}
-		
-	//
-	// ─── SEPARATOR COMMENT GENERATOR ────────────────────────────────────────────────
-	//
-	
-		export function GenerateSeparatorComment ( ) : string {
-			return '//' + MakeRepeat( ' ' + globalSeparatorValue , 5 );
-		}
-
-	// ────────────────────────────────────────────────────────────────────────────────
-	
-		function MakeLine ( size: number ) : string {
-			return MakeRepeat( boxHorizontalCharacter , size );
-		}
-
-	// ────────────────────────────────────────────────────────────────────────────────
-	
-		function MakeTitle ( text: string ) {
-			var result = '';
-			for ( var index = 0 ; index < text.length ; index++ ) {
-				result += ' ' + text[ index ];
-			}
-			return result.toUpperCase( );			
-		}
+		var errorPlaceHolder: 				string;
 		
 	// ────────────────────────────────────────────────────────────────────────────────
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
+
+
+
+
+
+//
+// ──────────────────────────────────────────────────────────────────── II ──────────
+//  :::::: C O R E   N A M E S P A C E : :  :   :    :     :        :          :
+// ──────────────────────────────────────────────────────────────────────────────
+//
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
+
+	module Core {
 		
-		function MakeRepeat ( character: string , size: number ) : string {
-			var text = '';
-			for ( var counter = 0 ; counter < size ; counter++ ) {
-				text += character;
-			}
-			return text;
-		}
-		
-	//
-	// ─── COMMENT INDENT APPLIER ─────────────────────────────────────────────────────
-	//
-		
-		/** 
-		 * Applies indentation on the comments
-		 */
-		export function ApplyIndentation ( comment: string ) : string {
-			var lines = comment.split( '\n' );
-			var result = '';
-			let countOfLines = lines.length;
-			for ( var couter = 0 ; couter < countOfLines ; couter++ ) {
-				result += MakeRepeat( globalIndentStringValue , globalIndentSizeValue ) + lines[ couter ];
-				if ( couter < countOfLines - 1 ) {
-					result += '\n';
+		//
+		// ─── SPECIAL CHARACTERS ─────────────────────────────────────────────────────────
+		//
+
+			const boxVerticalCharacter 		= '&boxv;';
+			const boxHorizontalCharacter 	= '&boxh;';
+			const boxTopLeftCharacter 		= '&boxdr;';
+			const boxTopRightCharacter 		= '&boxdl;';
+			const boxBottomLeftCharacter 	= '&boxur;';
+			const boxBottomRightCharacter	= '&boxul;';
+			
+		//
+		// ─── COMMENT CLASS GENERATOR ────────────────────────────────────────────────────
+		//
+
+			export function GenerateClassComment ( ) : string {
+				// First Info
+				var commnet: string;
+				
+				// Line One
+				commnet = languageMultiLineTopLeft + ' ' + MakeLine( globalSizeValue ) + ' ' + languageMultiLineTopRight + '\n';
+				
+				// Line Two
+				const titleText = MakeTitle( globalTextValue );
+				var	  inCaseOfOddNumber = '';
+				var   dots = '';
+				
+				for ( var counter = 1; counter < ( globalSizeValue - titleText.length ) / 2 ; counter++ ) {
+					dots += '&colon;';
 				}
+				
+				if ( ( globalSizeValue - titleText.length ) % 2 == 0 ) {
+					console.log( 'here');
+					inCaseOfOddNumber = '&colon;';
+				}
+				
+				commnet += languageMultiLineBottomLeft + ' ' + inCaseOfOddNumber + dots + titleText + ' ' + dots + ' ' + languageMultiLineTopRight + '\n' ;
+				
+				// Line Tree
+				commnet += languageMultiLineBottomLeft + ' ' + MakeLine( globalSizeValue ) + ' ' + languageMultiLineBottomRight;
+				
+				// Done!
+				console.log( commnet );
+				return commnet;
 			}
-			return result;
-		}
+			
+		//
+		// ─── FLAG COMMENT GENERATOR ─────────────────────────────────────────────────────
+		//
+
+			export function GenerateFlagCommet ( ) : string {
+				// Line 1
+				var comment = languageOneLine + '\n';
+				// Line 2
+				const text = MakeTitle( globalTextValue );
+				comment += languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , text.length + 40 ) + ' ' + Kary.Text.Numerics.Roman( globalIndexValue ) + ' ' + MakeRepeat( boxHorizontalCharacter , 10 ) + '\n';
+				// Line 3
+				comment += languageOneLine + '  ::::::' + text + ' : :  :   :    :     :        :          :\n';
+				// Line 4
+				comment += languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , 50 + text.length ) + '\n';
+				// Line 5
+				comment += languageOneLine
+				// Done
+				return comment;
+			}
+
+		//
+		// ─── LINE GENERATOR ─────────────────────────────────────────────────────────────
+		//
+
+			export function GenerateLineComment ( ) : string {
+				return languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , globalSizeValue );
+			}
+			
+		//
+		// ─── SUBLINE COMMENT GENERATOR ──────────────────────────────────────────────────
+		//
+
+			export function GenerateSubLineComment ( ) : string {
+				var comment = languageOneLine;
+				if ( globalSizeValue % 2 == 0 ) {
+					comment += ' ';
+				}
+				comment += MakeRepeat ( ' -' , globalSizeValue / 2 );
+				return comment;
+			}
+			
+		//
+		// ─── SECTION COMMENT GENERATOR ──────────────────────────────────────────────────
+		//
+
+			export function GenerateSectionComment (  ) : string {
+				// Line 1
+				var comment = languageOneLine + '\n';
+				// Line 2
+				comment += (
+					languageOneLine + ' ' + MakeRepeat( boxHorizontalCharacter , 3 ) + ' ' +
+					globalTextValue.toUpperCase( ) + ' ' + MakeRepeat( boxHorizontalCharacter , globalSizeValue - globalTextValue.length - 5 ) + '\n'
+				);
+				// Line 3
+				comment += languageOneLine;
+				// Done
+				return comment;	
+			}
+			
+		//
+		// ─── SUBSECTION COMMENT GENERATOR ───────────────────────────────────────────────
+		//
 		
-	// ────────────────────────────────────────────────────────────────────────────────
-}
+			export function GenerateSubSectionComment (  ) : string {
+				// Line 1
+				var comment = languageOneLine + '\n';
+				// Line 2
+				comment += languageOneLine + ' ' + '- -' + ' ' + globalTextValue.toLowerCase( );
+				var lineSize = ( globalSizeValue - globalTextValue.length - 5 ) / 2;
+				if ( lineSize % 2 == 0 ) {
+					comment += ' ' + MakeRepeat( ' -' , lineSize ) + '\n'; 
+				} else {
+					comment += MakeRepeat( ' -' , lineSize ) + '\n';
+				}
+				// Line 3
+				comment += languageOneLine;
+				// Done
+				return comment;	
+			}
+			
+		//
+		// ─── IS SECTION COMMENT GENERATOR ───────────────────────────────────────────────
+		//
+
+			export function GenerateInSectionComment ( ) : string {
+				var comment =  languageOneLine + '\n';
+				comment += languageOneLine + ' ' + globalTextValue.toUpperCase( ) + '\n';
+				comment += languageOneLine;
+				return comment;
+			}
+			
+		//
+		// ─── SEPARATOR COMMENT GENERATOR ────────────────────────────────────────────────
+		//
+		
+			export function GenerateSeparatorComment ( ) : string {
+				return '//' + MakeRepeat( ' ' + globalSeparatorValue , 5 );
+			}
+
+		// ────────────────────────────────────────────────────────────────────────────────
+		
+			function MakeLine ( size: number ) : string {
+				return MakeRepeat( boxHorizontalCharacter , size );
+			}
+
+		// ────────────────────────────────────────────────────────────────────────────────
+		
+			function MakeTitle ( text: string ) {
+				var result = '';
+				for ( var index = 0 ; index < text.length ; index++ ) {
+					result += ' ' + text[ index ];
+				}
+				return result.toUpperCase( );			
+			}
+			
+		// ────────────────────────────────────────────────────────────────────────────────
+			
+			function MakeRepeat ( character: string , size: number ) : string {
+				var text = '';
+				for ( var counter = 0 ; counter < size ; counter++ ) {
+					text += character;
+				}
+				return text;
+			}
+			
+		//
+		// ─── COMMENT INDENT APPLIER ─────────────────────────────────────────────────────
+		//
+			
+			/** 
+			 * Applies indentation on the comments
+			 */
+			export function ApplyIndentation ( comment: string ) : string {
+				var lines = comment.split( '\n' );
+				var result = '';
+				let countOfLines = lines.length;
+				for ( var couter = 0 ; couter < countOfLines ; couter++ ) {
+					result += MakeRepeat( globalIndentStringValue , globalIndentSizeValue ) + lines[ couter ];
+					if ( couter < countOfLines - 1 ) {
+						result += '\n';
+					}
+				}
+				return result;
+			}
+			
+		// ────────────────────────────────────────────────────────────────────────────────
+	}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
+
