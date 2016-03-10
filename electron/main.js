9757c796abcec20f6b98643efca3dd89d40076d4
Pouya Kary <k@karyfoundation.org>
2016-03-11 01:33:12 +0330

    cleaned

--- electron/main.js
+++ electron/main.js
@@ -31,7 +31,7 @@
 
 		mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
 		
-		mainWindow.webContents.openDevTools();
+		// mainWindow.webContents.openDevTools();
 
 		mainWindow.on( 'closed' , function( ) {
 			mainWindow = null;
