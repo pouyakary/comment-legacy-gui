1848ea9cef76fe46141bdb4b403e40c8a5627783
Pouya Kary <k@karyfoundation.org>
2016-04-22 01:55:34 +0430

    commented....

--- core/languages.ts
+++ core/languages.ts
@@ -6,76 +6,86 @@
 
 /// <reference path="comment.ts" />
 
-interface LanguageTemplate {
-	id: string;
-	name: string;
-	temp: LanguageCharacters;
-}
+//
+// ─── INTERFACES ─────────────────────────────────────────────────────────────────
+//
 
-interface LanguageCharacters {
-	ol: string;
-	tl: string;
-	tr: string;
-	bl: string;
-	br: string;
-}
+	interface LanguageTemplate {
+		id: string;
+		name: string;
+		temp: LanguageCharacters;
+	}
 
-module Languages {
-	export var LanguageTemplates: Array<LanguageTemplate> = [
-		{ 
-			'id': 'arendelle',
-			'name': 'Arendelle',
-			'temp': {
-				'ol': '--',
-				'tl': '(*',
-				'tr': '* ',
-				'bl': ' *',
-				'br': '*)',		
-			}
-		},
-		{
-			'id': 'c',
-			'name': 'C Family',
-			'temp' : {
-				'ol': '//',
-				'tl': '/*',
-				'tr': '* ',
-				'bl': ' *',
-				'br': '*/',		
-			}
-		},
-		{
-			'id': 'ruby',
-			'name': 'Ruby Like',
-			'temp': {
-				'ol': '#',
-				'tl': '#',
-				'tr': '#',
-				'bl': '#',
-				'br': '#',		
-			}
-		},
-		{
-			'id': 'applescript',
-			'name': 'AppleScript',
-			'temp': {
-				'ol': '--',
-				'tl': '--',
-				'tr': '--',
-				'bl': '--',
-				'br': '--',		
-			}
-		},
-		{
-			'id': 'make',
-			'name': 'Makefile',
-			'temp': {
-				'ol': '%',
-				'tl': '%',
-				'tr': '%',
-				'bl': '%',
-				'br': '%',		
-			}
-		},
-	]	
-}
+	interface LanguageCharacters {
+		ol: string;
+		tl: string;
+		tr: string;
+		bl: string;
+		br: string;
+	}
+	
+//
+// ─── LANGUAGES ──────────────────────────────────────────────────────────────────
+//
+
+	module Languages {
+		export var LanguageTemplates: Array<LanguageTemplate> = [
+			{ 
+				'id': 'arendelle',
+				'name': 'Arendelle',
+				'temp': {
+					'ol': '--',
+					'tl': '(*',
+					'tr': '* ',
+					'bl': ' *',
+					'br': '*)',		
+				}
+			},
+			{
+				'id': 'c',
+				'name': 'C Family',
+				'temp' : {
+					'ol': '//',
+					'tl': '/*',
+					'tr': '* ',
+					'bl': ' *',
+					'br': '*/',		
+				}
+			},
+			{
+				'id': 'ruby',
+				'name': 'Ruby Like',
+				'temp': {
+					'ol': '#',
+					'tl': '#',
+					'tr': '#',
+					'bl': '#',
+					'br': '#',		
+				}
+			},
+			{
+				'id': 'applescript',
+				'name': 'AppleScript',
+				'temp': {
+					'ol': '--',
+					'tl': '--',
+					'tr': '--',
+					'bl': '--',
+					'br': '--',		
+				}
+			},
+			{
+				'id': 'make',
+				'name': 'Makefile',
+				'temp': {
+					'ol': '%',
+					'tl': '%',
+					'tr': '%',
+					'bl': '%',
+					'br': '%',		
+				}
+			},
+		]	
+	}
+	
+// ────────────────────────────────────────────────────────────────────────────────
