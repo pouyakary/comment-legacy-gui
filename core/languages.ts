4d74bd5531707c1ffd2ed0ed9bd74a647d790c44
Pouya Kary <k@karyfoundation.org>
2016-05-16 18:40:04 +0430

    commented out

--- core/languages.ts
+++ core/languages.ts
@@ -30,83 +30,118 @@
 
 	module Languages {
 		export var LanguageTemplates: Array<LanguageTemplate> = [
-			{ 
-				'id': 'arendelle',
-				'name': 'Arendelle',
-				'temp': {
-					'ol': '--',
-					'tl': '(*',
-					'tr': '* ',
-					'bl': ' *',
-					'br': '*)',		
-				}
-			},
-			{
-				'id': 'c',
-				'name': 'C Family',
-				'temp' : {
-					'ol': '//',
-					'tl': '/*',
-					'tr': '* ',
-					'bl': ' *',
-					'br': '*/',		
-				}
-			},
-			{
-				'id': 'ruby',
-				'name': 'Ruby Like',
-				'temp': {
-					'ol': '#',
-					'tl': '#',
-					'tr': '#',
-					'bl': '#',
-					'br': '#',		
-				}
-			},
-			{
-				'id': 'applescript',
-				'name': 'AppleScript',
-				'temp': {
-					'ol': '--',
-					'tl': '--',
-					'tr': '--',
-					'bl': '--',
-					'br': '--',		
-				}
-			},
-			{
-				'id': 'make',
-				'name': 'Makefile',
-				'temp': {
-					'ol': '%',
-					'tl': '%',
-					'tr': '%',
-					'bl': '%',
-					'br': '%',		
-				}
-			},
-			{
-				'id': 'mathemathica',
-				'name': 'Mathemathica',
-				'temp': {
-					'ol': '(*',
-					'tl': '(*',
-					'tr': '*',
-					'bl': ' *',
-					'br': '*)',		
-				}
-			},
-			{
-				'id': 'assembly',
-				'name': 'Assembly',
-				'temp': {
-					'ol': ';',
-					'tl': ';',
-					'tr': ';',
-					'bl': ';',
-					'br': ';',		
-				}
-			},
+			
+			//
+			// ARENDELLE
+			//
+			
+				{ 
+					'id': 'arendelle',
+					'name': 'Arendelle',
+					'temp': {
+						'ol': '--',
+						'tl': '(*',
+						'tr': '* ',
+						'bl': ' *',
+						'br': '*)',		
+					}
+				},
+			
+			//
+			// C FAMILY (C, C#, JAVA, JAVASCRIPT, ETC. )
+			//			
+			
+				{
+					'id': 'c',
+					'name': 'C Family',
+					'temp' : {
+						'ol': '//',
+						'tl': '/*',
+						'tr': '* ',
+						'bl': ' *',
+						'br': '*/',		
+					}
+				},
+			
+			//
+			// RUBY AND PYTHON
+			//
+			
+				{
+					'id': 'ruby',
+					'name': 'Ruby & Python',
+					'temp': {
+						'ol': '#',
+						'tl': '#',
+						'tr': '#',
+						'bl': '#',
+						'br': '#',		
+					}
+				},
+				
+			//
+			// APPLESCRIPT
+			//
+			
+				{
+					'id': 'applescript',
+					'name': 'AppleScript',
+					'temp': {
+						'ol': '--',
+						'tl': '--',
+						'tr': '--',
+						'bl': '--',
+						'br': '--',		
+					}
+				},
+				
+			//
+			// MAKE FILE
+			//
+			
+				{
+					'id': 'make',
+					'name': 'Makefile',
+					'temp': {
+						'ol': '%',
+						'tl': '%',
+						'tr': '%',
+						'bl': '%',
+						'br': '%',		
+					}
+				},
+				
+			//
+			// WOLFRAM LANGUAGE, MATHEMATHICA
+			//
+			
+				{
+					'id': 'mathemathica',
+					'name': 'Mathemathica',
+					'temp': {
+						'ol': '(*',
+						'tl': '(*',
+						'tr': '*',
+						'bl': ' *',
+						'br': '*)',		
+					}
+				},
+
+			//
+			// ASSEMBLY
+			//
+			
+				{
+					'id': 'assembly',
+					'name': 'Assembly',
+					'temp': {
+						'ol': ';',
+						'tl': ';',
+						'tr': ';',
+						'bl': ';',
+						'br': ';',		
+					}
+				},
 		]	
 	}
 	
