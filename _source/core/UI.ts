a78abe8e27b753c5f0ab124c57096e2067fa4b5d
Pouya Kary <k@karyfoundation.org>
2017-03-27 19:36:14 +0430

    copy now works

--- _source/core/UI.ts
+++ _source/core/UI.ts
@@ -134,7 +134,7 @@ module UI {
         export function CopyHTMLCode ( ) {
             let commentString = GenerateCommentHTMLCode( );
             if ( commentString != null )
-                ElectronCopy( GenerateComment( ) );
+                SetClipboardText( GenerateComment( ) );
         }
 
     //
@@ -295,7 +295,7 @@ module UI {
 
         export function CopyComment ( ) {
             if ( lastGeneratedCommentText !== '' ) {
-                ElectronCopy( lastGeneratedCommentText.replace( /^\s+/, '' ) );
+                SetClipboardText( lastGeneratedCommentText.replace( /^\s+/, '' ) );
             }
         }
 
@@ -859,6 +859,56 @@ module UI {
 
         export function AddKeyBoardEvents ( ) {
             document.addEventListener( 'keydown', OnKeyEvent );
+        }
+
+        function SetClipboardText ( text: string ) {
+            var id = "clipboard-textarea-hidden-id";
+            var existsTextarea = <HTMLTextAreaElement> document.getElementById( id );
+
+            if(!existsTextarea){
+                console.log( "Creating textarea" );
+                var textarea = document.createElement( "textarea" );
+                textarea.id = id;
+                // Place in top-left corner of screen regardless of scroll position.
+                textarea.style.position = 'fixed';
+                textarea.style.top = "0";
+                textarea.style.left = "0";
+
+                // Ensure it has a small width and height. Setting to 1px / 1em
+                // doesn't work as this gives a negative w/h on some browsers.
+                textarea.style.width = '1px';
+                textarea.style.height = '1px';
+
+                // We don't need padding, reducing the size if it does flash render.
+                textarea.style.padding = "0";
+
+                // Clean up any borders.
+                textarea.style.border = 'none';
+                textarea.style.outline = 'none';
+                textarea.style.boxShadow = 'none';
+
+                // Avoid flash of white box if rendered for any reason.
+                textarea.style.background = 'transparent';
+                document.querySelector( "body" ).appendChild( textarea );
+                console.log( "The textarea now exists :)" );
+                existsTextarea = <HTMLTextAreaElement> document.getElementById( id );
+            } else {
+                console.log( "The textarea already exists :3" )
+            }
+
+            existsTextarea.value = text;
+            existsTextarea.select( );
+
+            try {
+                var status = document.execCommand( 'copy' );
+                if ( !status ) {
+                    console.error( "Cannot copy text" );
+                } else {
+                    console.log( "The text is now on the clipboard" );
+                }
+            } catch ( err ) {
+                console.log( 'Unable to copy.' );
+            }
         }
 
     //
